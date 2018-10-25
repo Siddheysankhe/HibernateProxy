76ea27a363fd8770a11f520eaccf0446df1b4b34
siddhey <siddhey.sankhe@kyepot.com>
2018-10-25 20:58:55 +0530

    changed for pet

--- src/test/java/com/siddhey/DemoProxy/DemoProxyApplicationTests.java
+++ src/test/java/com/siddhey/DemoProxy/DemoProxyApplicationTests.java
@@ -1,19 +1,14 @@
 package com.siddhey.DemoProxy;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.siddhey.DemoProxy.Entitites.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,11 +20,6 @@ public class DemoProxyApplicationTests {
 	@Test
 	@Transactional
 	public void contextLoads() {
-		final User tom = new User("Tom", "Admin");
-		entityManager.persist(tom);
-		User tom1 = entityManager.getReference(User.class, tom.getId());
-		assertThat(tom1).isInstanceOf(User.class);
-		assertThat(tom).isSameAs(tom1);
 	}
 
 }
