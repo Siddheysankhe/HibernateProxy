c9b183d2f575f00d99a3420b5d5e0affc2f61354
siddhey <siddhey.sankhe@kyepot.com>
2018-10-25 22:37:21 +0530

    difference between getReference and find methods of entity manager

--- src/test/java/com/siddhey/DemoProxy/DemoProxyApplicationTests.java
+++ src/test/java/com/siddhey/DemoProxy/DemoProxyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.siddhey.DemoProxy;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.hibernate.Hibernate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.siddhey.DemoProxy.Entitites.Post;
+import com.siddhey.DemoProxy.Entitites.PostComment;
 
 
 @RunWith(SpringRunner.class)
@@ -16,10 +26,62 @@ public class DemoProxyApplicationTests {
 	
 	@PersistenceContext
 	private EntityManager entityManager;
+	
+	private static final Logger logger = LoggerFactory.getLogger(DemoProxyApplicationTests.class);
 
+//	@Test
+//	@Transactional
+//	public void contextLoads() {
+//		
+//		//find
+//		Post post = new Post();
+//		post.setId(1L);
+//		post.setTitle("Siddhey Sankhe");
+//		
+//		entityManager.persist(post);
+//		
+//		Post post1 = entityManager.find(Post.class, 1L);
+//		assertEquals("Siddhey Sankhe", post1.getTitle());
+//		
+//		PostComment comment = new PostComment();
+//		comment.setReview("Awesome book");
+//		
+//		Post post3 = entityManager.getReference(Post.class, 1L);
+//		comment.setPost(post3);
+//		 
+//		entityManager.persist(comment);
+//		
+//		//getReference
+//		
+//		PostComment comment1 = new PostComment();
+//		comment.setReview("Just awesome!");
+//		 
+//		Post post4 = entityManager.getReference(Post.class, 1L);
+//		comment.setPost(post4);
+//		 
+//		entityManager.persist(comment1);
+//		
+//	}
+	
 	@Test
 	@Transactional
-	public void contextLoads() {
+	public void proxyTest() {
+		Post post = new Post();
+		post.setId(1L);
+		post.setTitle("Siddhey Sankhe");
+		entityManager.persist(post);
+		
+		PostComment comment = new PostComment();
+		comment.setId(1L);
+		comment.setReview("Awesome");
+		comment.setPost(post);
+		entityManager.persist(comment);
+		
+		PostComment comment1 = entityManager.getReference(PostComment.class, 1L);
+		
+		assertFalse(comment.equals(comment1));
+		
+		assertTrue(comment.equals(Hibernate.unproxy(comment1)));
 	}
 
 }
